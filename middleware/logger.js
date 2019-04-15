c9b1cf66602d88d6f063c1f1dba0d656f1da5a07
james <jamestang_@outlook.com>
2019-04-15 12:10:52 +0100

    add middleware folder

---
+++ middleware/logger.js
@@ -0,0 +1,14 @@
+const moment = require("moment");
+
+//middleware
+//middleware functions are functions that have access to the request and response object.
+const logger = (req, res, next) => {
+  console.log(
+    `${req.protocol}://${req.get("host")}${
+      req.originalUrl
+    }: ${moment().format()}`
+  );
+  next();
+};
+
+module.exports = logger;
